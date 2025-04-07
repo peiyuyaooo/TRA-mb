57be505bbd328e15753d38497c9c1100698801aa
peiyuyaooo <peiyuyaooo@gmail.com>
2025-04-07 19:21:07 +0800

    ticket information card with swiper and js ()

---
+++ js/script.js
@@ -0,0 +1,30 @@
+ new Swiper('.card-wrapper', {
+    loop: true,
+    paceBetween: 30,
+  
+    // Pagination Bullets
+    pagination: {
+      el: '.swiper-pagination',
+       clickable: true,
+       dynamicBullets: true, 
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    }
+  }); 
